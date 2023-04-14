f97addce8debd13d35ab01e6eb76159b40b20b09
chika918 <113702101+chika918@users.noreply.github.com>
2023-04-14 23:17:18 +0900

    3-3

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Practice2</title>
+</head>
+<body>
+  <!-- ①フォームを作成しましょう -->
+
+  <form action="result.php" method="post">
+    好きな名前を入れてください<br>
+    <input type="text" name="name">
+    <br>
+    1〜6の中で好きな数字を選んでください<br>
+    <select name="num">
+    <option>1</option>
+    <option>2</option>
+    <option>3</option>
+    <option>4</option>
+    <option>5</option>
+    <option>6</option>
+    </select>
+    <br>
+    <input type="submit">
+  </form>
+
+  <!-- ここにフォームを記述してください -->
+  </form>
+</body>
+</html>
---
+++ result.php
@@ -0,0 +1,41 @@
+<?php
+
+  //②フォームからのデータを受け取ります
+
+  $name = $_POST['name'];
+  $num = $_POST['num'];
+
+  //③受け取った数字に1~6までのランダムな数字を掛け合わせて
+  //変数に入れてください
+  //$rand = mt_rand(1, 6);
+  //$num = $num * $rand;
+  $num = $num * mt_rand(1, 6);
+  //echo $num;
+
+  //④掛け合わせた数字の結果によっておみくじを選び、変数に入れます
+  $result;
+  if($num >= 37){
+    $result = "残念";
+  }elseif($num >= 26){
+    $result = "大吉";
+  }elseif($num >= 21){
+    $result = "中吉";
+  }elseif($num >= 16){
+    $result = "吉";
+  }elseif($num >= 11){
+    $result = "小吉";
+  }elseif($num >= 1){
+    $result = "凶";
+  }
+  //⑤今日の日付と、名前、番号、おみくじ結果を表示しましょう
+
+  echo date("Y-m-d H:i:s", time());
+  echo '<br>';
+  echo "名前は",$name,"です。";
+  echo '<br>';
+  echo "番号は",$num,"です。";
+  echo '<br>';
+  echo "結果は",$result,"です。";
+
+  date_default_timezone_set('Asia/Tokyo');
+  ?>
